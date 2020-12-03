f1fec9fe52b0887fe583956068b428cfd30a74ef
Joel Degerman <joel@joeldegerman.se>
2020-12-03 21:16:01 +0100

    test

--- bot.js
+++ bot.js
@@ -6,18 +6,18 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+
 let channel;
 
-
 client.on("message", (msg) => {
-    if(msg.content === "hej") {
-        msg.reply("Hej")
-    }
-})
+  if (msg.content === "hej") {
+    msg.reply("Hej!!");
+  }
+});
 
 client.login(process.env.BOT_TOKEN).then(() => {
-    channel = client.channels.cache.get("784086230769336353");
-}) 
+  channel = client.channels.cache.get("784086230769336353");
+});
 
 let monday = new cron.CronJob("50 8 * * 1", () => {
   channel.send("Kom ihåg lektionen med Ulf kl. 09.00");
@@ -27,8 +27,11 @@ let friday = new cron.CronJob("50 8 * * 5", () => {
 });
 
 let test = new cron.CronJob("* * * * *", () => {
-    channel.send("**```Kom ihåg lektionen med Ulf kl. 09.00```**");
-  });
+  channel.send("**```Kom ihåg lektionen med Ulf kl. 09.00```**");
+  const embed = new Discord.MessageEmbed()
+  .setColor("#0099ff")
+  .setTitle("Lektion")
+});
 monday.start();
 friday.start();
-test.start()
+test.start();
