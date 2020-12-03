f749dfd2f2efdcb82d4142b613362a11eb933e1d
Joel Degerman <joel@joeldegerman.se>
2020-12-03 21:25:17 +0100

    Almost done

--- bot.js
+++ bot.js
@@ -6,23 +6,26 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-
 let channel;
 
 client.on("message", (msg) => {
   if (msg.content === "hej") {
     msg.reply("Hej!!");
     const embed = new Discord.MessageEmbed()
-  .setColor("#0099ff")
-  .setTitle("Lektion")
-  .setDescription("Kom ihåg lektionen kl 09.00")
-  channel.send(embed)
+      .setColor("#0099ff")
+      .setTitle("Lektion")
+      .setDescription("Kom ihåg lektionen kl 09.00");
+    channel.send(embed);
   }
 });
 
+const embed = new Discord.MessageEmbed()
+  .setColor("#0099ff")
+  .setTitle("Lektion")
+  .setDescription("Kom ihåg lektionen kl 09.00");
+
 client.login(process.env.BOT_TOKEN).then(() => {
   channel = client.channels.cache.get("784086230769336353");
-  
 });
 
 let monday = new cron.CronJob("50 8 * * 1", () => {
@@ -33,12 +36,7 @@ let friday = new cron.CronJob("50 8 * * 5", () => {
 });
 
 let test = new cron.CronJob("* * * * *", () => {
-  channel.send("**```Kom ihåg lektionen med Ulf kl. 09.00```**");
-  const embed = new Discord.MessageEmbed()
-  .setColor("#0099ff")
-  .setTitle("Lektion")
-  .setDescription("Kom ihåg lektionen kl 09.00")
-  channel.send(embed)
+  channel.send(embed);
 });
 monday.start();
 friday.start();
