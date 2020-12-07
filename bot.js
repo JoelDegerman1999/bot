503c4a344531cbfa2462db5bbebec43657d254ac
Joel Degerman <joel@joeldegerman.se>
2020-12-07 08:59:33 +0100

    Testing

--- bot.js
+++ bot.js
@@ -6,29 +6,32 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const command = require("./command");
-
-let channel;
-
 const embed = new Discord.MessageEmbed()
   .setColor("#0099ff")
   .setTitle("Lektion")
   .setDescription("Kom ihåg lektionen kl 09.00");
 
+let chan;
 
 let monday = new cron.CronJob("50 8 * * 1", () => {
-  client.login(process.env.BOT_TOKEN).then(() => {
-    let channel = client.channels.cache.get("613364620639469600");
-    channel.send("@everyone")
-    channel.send(embed)
-  })
-});
-let friday = new cron.CronJob("50 8 * * 5", () => {
-  client.login(process.env.BOT_TOKEN).then(() => {
-    let channel = client.channels.cache.get("613364620639469600");
-    channel.send("@everyone")
-    channel.send(embed)
-  })
+    chan.send("@everyone");
+    chan.send(embed);
 });
 monday.start();
+let friday = new cron.CronJob("50 8 * * 5", () => {
+  cha.send("@everyone");
+  cha.send(embed);
+});
 friday.start();
+
+let test = new cron.CronJob("5 9 * * 1", () => {
+  cha.send("@everyone");
+  cha.send(embed);
+});
+test.start
+
+
+
+client.login(process.env.BOT_TOKEN).then(() => {
+  chan = client.channels.cache.get("784852145139875861");
+});
